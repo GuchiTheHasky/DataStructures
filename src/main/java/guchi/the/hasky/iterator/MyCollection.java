30e28d5b5b70aee21e5482abe17080a0aba30500
GuchiTheHasky <110381425+GuchiTheHasky@users.noreply.github.com>
2023-02-22 01:54:30 +0200

    Delete src/main/java/guchi/the/hasky/iterator directory

--- src/main/java/guchi/the/hasky/iterator/MyCollection.java
+++
@@ -1,5 +0,0 @@
-package guchi.the.hasky.iterator;
-
-public interface MyCollection<T> {
-    MyIterator<T> getIterator();
-}
--- src/main/java/guchi/the/hasky/iterator/MyIterator.java
+++
@@ -1,6 +0,0 @@
-package guchi.the.hasky.iterator;
-
-public interface MyIterator <T> {
-    boolean hasNext();
-    T next();
-}
